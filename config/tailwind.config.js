d3952fa0c13e622586f338700b849f83ed6de9ba
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-04-11 17:29:11 +0200

    I added stimulus and navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -8,6 +11,13 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: "#ff5a5f",
+        dark: "#ff385c",
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
