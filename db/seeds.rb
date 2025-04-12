299de44ea95c9bdeca8af2a088e530968918f497
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-04-12 12:58:05 +0200

    working on home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+properties = Property.create!(
+  name: 'sample property',
+  description: 'amazing',
+  headline: 'On sale',
+  address_1: 'hale street',
+  address_2: '2331xc',
+  city: 'Amsterdam',
+  state: 'South Holland',
+  country:'the Netherlands',
+)
+
+properties = Property.create!(
+  name: 'sample property 2',
+  description: 'Nice',
+  headline: 'On sale',
+  address_1: 'Nabaa',
+  address_2: '331zz',
+  city: 'Beirut',
+  state: 'Maten',
+  country:'Lebanon',
+)
